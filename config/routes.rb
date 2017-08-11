1af3d4de8e7513f044a4d47337161435b858b289
Christopher Turpin <cjturpin1@gmail.com>
2017-08-11 12:10:59 -0400

    makes routes prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'statuses/index'
+  get 'statuses',
+to: 'statuses#index',
+page: 1
+
+get 'statuses/page/:page',
+to: 'statuses#index'
 
   get 'statuses/show'
 
   get 'statuses/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
